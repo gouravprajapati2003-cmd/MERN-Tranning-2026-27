e1612aac6ce541fc868f926281ac7b126a66ebd1
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-06-10 11:42:17 +0530

    Add files via upload

---
+++ 10-jun-2026/Transaction.js
@@ -0,0 +1,30 @@
+let transactions = [
+    {
+        transactionid: 't001',
+        amount: 1000,
+        userid: 'user@yopmail001',
+        date: '10-jun-26'
+    },
+    {
+        transactionid: 't002',
+        amount: 2000,
+        userid: 'user@yopmail002',
+        date: '11-jun-26'
+    },
+    {
+        transactionid: 't003',
+        amount: 3000,
+        userid: 'user@yopmail003',
+        date: '12-jun-26'
+    },
+    {
+        transactionid: 't004',
+        amount: 4000,
+        userid: 'user@yopmail004',
+        date: '13-jun-26'
+    }
+]
+let filterdata = transactions.map((transaction) => {
+    return 'Transaction Id = ' + transaction.transactionid + ", " + 'User Id = ' + transaction.userid
+})
+console.log(filterdata)
