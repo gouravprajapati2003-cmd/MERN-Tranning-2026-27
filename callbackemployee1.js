f1525845ca8e04ed384159730b300d7608188a98
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-06-15 22:12:59 +0530

    Add files via upload

---
+++ callbackemployee1.js
@@ -0,0 +1,67 @@
+// we will have 10 emp having property 
+//eid, ename, mobileno, post
+// wer will search an employee based on id which will take 5 second
+// after getting the employee we will fetch the bssic salary of that particular employee which will take 3 second
+// after getting the basic salary we will calculate the gross salary of that employee
+let employees = [
+    {
+        ename: 'Aman',
+        eid: 'e001',
+        post: 'Worker',
+        mobileno: 9090987600
+    },
+    {
+        ename: 'Raju',
+        eid: 'e002',
+        post: 'Developer',
+        mobileno: 9039944884
+    },
+    {
+        ename: 'Seema',
+        eid: 'e003',
+        post: 'HR',
+        mobileno: 7237502776
+    },
+    {
+        ename: 'Gourav Kumar',
+        eid: 'e004',
+        post: 'CEO',
+        mobileno: 7969698590
+    },
+    {
+        ename: 'Tanisha',
+        eid: 'e005',
+        post: 'Manager',
+        mobileno: 6396000023
+    },
+    {
+        ename: 'Fardeen',
+        eid: 'e006',
+        post: 'Project Manager',
+        mobileno: 7597583959
+    },
+    {
+        ename: 'Divya',
+        eid: 'e007',
+        post: 'Secretory',
+        mobileno: 8990998887
+    },
+    {
+        ename: 'Reena',
+        eid: 'e008',
+        post: 'Employee',
+        mobileno: 9898002314
+    },
+    {
+        ename: 'Shivani',
+        eid: 'e009',
+        post: 'Boss',
+        mobileno: 890003245
+    },
+    {
+        ename: 'Mohit',
+        eid: 'e010',
+        post: 'Technician Head',
+        mobileno: 8899007766
+    },
+]
