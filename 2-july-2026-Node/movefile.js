dd7ceda5e7dafef49f66b4bd3cc78300a8bbab34
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-07-07 22:43:28 +0530

    Add files via upload

---
+++ 2-july-2026-Node/movefile.js
@@ -0,0 +1,23 @@
+const math = require('./math');
+//console.log(math)
+let result = math.add(60, 40);
+console.log(result)
+result = math.subtract(60, 40);
+console.log(result)
+
+// const area = require('./area');
+// console.log(area);
+// let result = area.circle(5)
+// console.log(result)
+// result = area.rectangle(3, 4);
+// console.log(result)
+// result = area.triangle(4, 5)
+// console.log(result);
+
+// const volume = require('./volumes/volume');
+// console.log(volume);
+// let result = volume.volume_cone(90,45);
+// console.log(result);
+// result = volume.volume_cube(5);
+// console.log(result)
+
