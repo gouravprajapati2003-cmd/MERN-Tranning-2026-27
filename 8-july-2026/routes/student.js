d331ec8eb0a29b4ac08499e481cc5085876eb365
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-07-13 21:43:56 +0530

    Add files via upload

--- 8-july-2026/routes/student.js
+++ 8-july-2026/routes/student.js
@@ -1,59 +1,31 @@
 const express = require('express')
 const route = express.Router()
-const fs = require('fs');
+const StudentController = require('../controllers/StudentController')
 const students = require('../student.json')
 
 
 // get -> /students http://localhost:3000/students
 route.get('/students', (req, res) => {
-    //res.json(students)
+    StudentController.getStudents(req, res);
 })
 
 // get through by id, http://localhost:3000/student/3
 route.get('/student/:rollNo', (req, res) => {
-    // let rollNo = req.params.rollNo;
-    // let student = students.find((student) => student.rollNo == rollNo)
-    // res.json(student);
+   StudentController.getStudentById(req, res);
 })
 
 // post method http://localhost:3000/add/student
 route.post('/add/student', (req, res) => {
-    // console.log(req.body);
-    // students.push(req.body);
-    // fs.writeFile('student.json', JSON.stringify(students), (err) => {
-    //     if (err) {
-    //         console.log("Data not added...")
-    //     } else {
-    //         res.end("Data added Successfully...");
-    //     }
-    // })
+    StudentController.addStudent(req, res);
 })
 
 // put method http://localhost:3000/edit/student/31
 route.put('/edit/student/:rollNo', (req, res) => {
-    let rollNo = req.params.rollNo
-    let index = students.findIndex((student) => student.rollNo == rollNo);
-    students[index].email = "vinodsharma231@gmail.com";
-    fs.writeFile('student.json', JSON.stringify(students), (err) => {
-        if (err) {
-            console.log("Data not updated...")
-        } else {
-            res.end("Data updated Successfully...");
-        }
-    })
+    StudentController.editStudent(req, res);
 })
 // delete method http://localhost:3000/delete/student/31
 route.delete('/delete/student/:rollNo', (req, res) => {
-    let rollNo = req.params.rollNo
-    let index = students.findIndex((student) => student.rollNo == rollNo);
-    students.splice(index, 1)
-    fs.writeFile('student.json', JSON.stringify(students), (err) => {
-        if (err) {
-            console.log("Data not Deleted...")
-        } else {
-            res.end("Data Deleted Successfully...");
-        }
-    })
+    StudentController.deleteStudent(req, res);
 })
 
 module.exports = route;
