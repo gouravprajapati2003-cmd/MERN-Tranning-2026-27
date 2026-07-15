2be722394faac9e7853db6cb952dedf8d2f79d95
gouravprajapati2003-cmd <gouravprajapati8132@gmail.com>
2026-07-15 09:47:30 +0530

    Add files via upload

--- 13-july-2026/index.js
+++ 13-july-2026/index.js
@@ -3,8 +3,10 @@ const app = express()
 const connect = require('./connection');
 app.use(express.json())
 const student = require('./routes/studentRoutes')
+const mobile = require('./routes/mobileRoutes');
 connect();
 app.use(student);
+app.use(mobile);
 app.listen(3000, (err) => {
     if (err) {
         console.log(err)
